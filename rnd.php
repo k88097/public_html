b2ef2a892ba8e7914abad399916d567a6ddf8fa5
k88097 <46487354+k88097@users.noreply.github.com>
2020-08-04 15:28:36 +0800

    R/D Centers Finished

---
+++ rnd.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>R/D Centers</title>
+
+    <!-- import Css Javascript jQuery -->
+    <?php include "head.html"; ?>
+</head>
+
+<body>
+    <!-- import Header -->
+    <?php include "header.html"; ?>
+
+    <div class="container" id="contact">
+        <div class="card">
+            <div class="card-header text-left">
+                <h2>R/D Centers</h2>
+            </div>
+            <div class="card-body">
+                <p class="card-title" id="rnd-title">
+                    We have more than 8 Research & Development Centers available to meet your needs and reduce your research costs.
+                    Learn from our experts and work with our worldwide network of medical industries and institute.
+                </p>
+                <!-- Lab -->
+                <div class="row">
+                    <div class="col-md-4 align-self-center" id="rnd-image">
+                        <img src="images/rnd/lab.jpg" alt="lab">
+                    </div>
+                    <div class="col-md-7 align-self-center" id="rnd-list">
+                        <ul>
+                            <li>#1 Outstanding Asia's R&D Center for Membrane Technology</li>
+                            <li>Top CMT in Membrane Society</li>
+                            <li>First Asia SCI Publications</li>
+                            <li>18+ laboratories</li>
+                            <li>Locate at the excellent location </li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- R&D Center -->
+                <div class="row">
+                    <div class="col-md-4 align-self-center" id="rnd-image">
+                        <img src="images/rnd/rd.jpg" alt="rnd">
+                    </div>
+                    <div class="col-md-7 align-self-center" id="rnd-list">
+                        <ul>
+                            <li>Asia's #1 National Nanotechnology Research Center</li>
+                            <li>Unique BME Technology Resources</li>
+                            <li>Funds Research and Innovation</li>
+                            <li>
+                                Molding Injection R& D Resource Center
+                                <ul>
+                                    <li>Mold desing by computer-aided-enginnering</li>
+                                    <li>High speed thin-wall</li>
+                                    <li>Gas/Liquid Assisted</li>
+                                    <li>Co-injection molding</li>
+                                    <li>Microcelluar</li>
+                                    <li>Precise Monitoring Technology for Injection Molding</li>
+                                    <li>Microcelluar Injection Molding</li>
+                                    <li>Etc...</li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- import Footer -->
+    <?php include "footer.html"; ?>
+</body>
+
+</html>
