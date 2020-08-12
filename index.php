0acc687946c8ad817c250b93659e96009dcba42c
k88097 <46487354+k88097@users.noreply.github.com>
2020-08-12 11:49:27 +0800

    test cache

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shianglong</title>
+    <?php
+    header("Cache-Control: public, max-age = 31536000");
+    ?>
 </head>
 
 <body>
